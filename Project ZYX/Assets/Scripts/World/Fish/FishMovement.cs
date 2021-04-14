b735cc619e32c456bd19f388e2b8cd30b38fcc19
centroid715 <lalalalic92@gmail.com>
2021-04-14 13:01:29 +0200

    Fiskar funkar nu

---
+++ Project ZYX/Assets/Scripts/World/Fish/FishMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishMovement : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed;
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "FishDestroyer")
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * Time.deltaTime * movementSpeed;
+    }
+}
---
+++ Project ZYX/Assets/Scripts/World/Fish/FishSpawner.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishSpawner : MonoBehaviour
+{
+    [Header("Spawn position")]
+    [SerializeField] private float xposMin;
+    [SerializeField] private float yposMin;
+    [SerializeField] private float zposMin;
+    [Space(20)]
+    [SerializeField] private float xposMax;
+    [SerializeField] private float yposMax;
+    [SerializeField] private float zposMax;
+
+    [Header("Small")] 
+    [SerializeField] private GameObject fishSmall;
+    [SerializeField] private int ammountSmall;
+    [SerializeField] private float minWaitTimeSmall;
+    [SerializeField] private float maxWaitTimeSmall;
+
+    [Header("Medium")]
+    [SerializeField] private GameObject fishMedium;
+    [SerializeField] private int ammountMedium;
+    [SerializeField] private float minWaitTimeMedium;
+    [SerializeField] private float maxWaitTimeMedium;
+
+    [Header("Big")]
+    [SerializeField] private GameObject fishBig;
+    [SerializeField] private int ammountBig;
+    [SerializeField] private float minWaitTimeBig;
+    [SerializeField] private float maxWaitTimeBig;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnFishSmallCO());
+        StartCoroutine(SpawnFishMediumCO());
+        StartCoroutine(SpawnFishBigCO());
+    }
+
+    #region Small
+    IEnumerator SpawnFishSmallCO()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minWaitTimeSmall, maxWaitTimeSmall));
+            SpawnFishSmall();
+        }
+    }
+    private void SpawnFishSmall()
+    {
+
+        for (int i = 0; i < ammountSmall; i++)
+        {
+            Vector3 randomizedPos = new Vector3(Random.Range(xposMin, xposMax), Random.Range(yposMin, yposMax), Random.Range(zposMin, zposMax));
+            Instantiate(fishSmall, randomizedPos, gameObject.transform.rotation);
+        }
+    }
+    #endregion
+
+    #region Medium
+    IEnumerator SpawnFishMediumCO()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minWaitTimeMedium, maxWaitTimeMedium));
+            SpawnFishMedium();
+        }
+    }
+    private void SpawnFishMedium()
+    {
+        for (int i = 0; i < ammountMedium; i++)
+        {
+            Vector3 randomizedPos = new Vector3(Random.Range(xposMin, xposMax), Random.Range(yposMin, yposMax), Random.Range(zposMin, zposMax));
+            Instantiate(fishMedium, randomizedPos, gameObject.transform.rotation);
+        }
+    }
+    #endregion
+    
+    #region Big
+    IEnumerator SpawnFishBigCO()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minWaitTimeBig, maxWaitTimeBig));
+            SpawnFishBig();
+        }
+    }
+    private void SpawnFishBig()
+    {
+        for (int i = 0; i < ammountBig; i++)
+        {
+            Vector3 randomizedPos = new Vector3(Random.Range(xposMin, xposMax), Random.Range(yposMin, yposMax), Random.Range(zposMin, zposMax));
+            Instantiate(fishBig, randomizedPos, gameObject.transform.rotation);
+        }
+    }
+    #endregion
+}
