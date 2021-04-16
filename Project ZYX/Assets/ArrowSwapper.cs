a74ef32a8a5fc835bb5adcd41171ab7fd1e554cc
centroid715 <lalalalic92@gmail.com>
2021-04-16 12:20:59 +0200

    arrows funkar perfekt

---
+++ Project ZYX/Assets/ArrowSwapper.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowSwapper : MonoBehaviour
+{
+    [SerializeField] private Texture2D arrow;
+    [SerializeField] private Texture2D hand;
+    public void SwapToArrow()
+    {
+        Cursor.SetCursor(arrow, Vector3.zero, CursorMode.ForceSoftware);
+    }
+    public void SwapToHand()
+    {
+        Cursor.SetCursor(hand, Vector3.zero, CursorMode.ForceSoftware);
+    }
+}
